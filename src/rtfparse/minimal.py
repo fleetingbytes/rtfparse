6df112da0cfe5d9aabcbb350fbd709e033cdeec6
Sven Siegmund <sven.siegmund@iav.de>
2021-01-04 08:43:27 +0100

    Starting API rewrite

--- src/rtfparse/minimal.py
+++ src/rtfparse/minimal.py
@@ -6,13 +6,12 @@ from rtfparse.parsers import Rtf_Parser
 from rtfparse.renderers import encapsulated_html
 
 
-parser = Rtf_Parser()
-renderer = encapsulated_html.Encapsulated_HTML()
 source_file = pathlib.Path(r"D:\trace\email\test_mail_sw_release.rtf")
 target_file = pathlib.Path(r"D:\trace\email\extracted_with_rtfparse.html")
 
 
-with open(source_file, mode="rb") as rtf_file:
-    parser.parse_file(rtf_file, default_encoding="cp1252")
-    with open(target_file, mode="w", encoding="utf-8") as html_file:
-        renderer.render(parser.parsed, target_file)
+parser = Rtf_Parser(rtf_file=source_file)
+parsed = parser.parse_file()
+
+renderer = encapsulated_html.Encapsulated_HTML()
+renderer.render(rtf_structure=parsed, target_file=target_file)
